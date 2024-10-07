cfec7fcb9ab542e6fe7b7e75b505982e7c43f81b
akosimajo <89579210+akosimajo@users.noreply.github.com>
2024-10-08 00:12:04 +0800

    added login page

---
+++ photo-album/src/Components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './LoginSignup.css'
+
+import email_icon from '../../Components/Assets/email.png';
+import password_icon from '../../Components/Assets/password.png';
+
+
+const LoginSignup = () => {
+
+  return (
+    <div>
+      <div className="container">
+        <div className="inputs">
+          <div className="input">
+            <img src={email_icon} alt="user"/>
+            <input type="email" placeholder='Email'/>
+          </div>
+          <div className="input">
+            <img src={password_icon} alt="user"/>
+            <input type="password" placeholder='Password'/>
+          </div>
+        </div>
+        <div className="submit-container">
+          <div className="submit">Log In</div>
+        </div>
+        <div className="forgot-password">
+          <a href="#" onClick={(e) => e.preventDefault()}>Forgot Password?</a>
+        </div>
+      </div>
+      <h1 className="title">
+        University of Batangas<br/>
+        Photo Album
+      </h1>
+    </div>
+  );
+}
+
+export default LoginSignup;
